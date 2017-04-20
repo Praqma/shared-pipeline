1612da9ace61a7207f6336bc29890f1fa1a9dee1
Mads Nielsen <man@praqma.net>
2017-04-20 15:08:46 +0200

    CLosure

--- src/com/praqma/StageGenerator.groovy
+++ src/com/praqma/StageGenerator.groovy
@@ -4,4 +4,8 @@ class StageGenerator {
     static def getStageClosure() {
         return { stage("hello") { echo 'foobar' } }
     }
+
+    static def echoThis(context) {
+        context.echo "HELLO FROM CONTEXT"
+    }
 }
