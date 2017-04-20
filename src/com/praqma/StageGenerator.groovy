caba5234a79fae99c64dbb066f2016e98eeb5e0a
Mads Nielsen <man@praqma.net>
2017-04-20 14:45:29 +0200

    Global

--- src/com/praqma/StageGenerator.groovy
+++ src/com/praqma/StageGenerator.groovy
@@ -1,19 +1,5 @@
 package com.praqma
 
-// point in 3D space
 class StageGenerator {
-    float x,y,z;
+
 }
-
-/*
-class StageGenerator implements Serializable {
-
-    static def getStage() {
-        return "stage('my-stage') { echo 'helloooooeee' } "
-    }
-
-    static def Closure getStageAsClosure() {
-        return { stage("hahah") { steps { echo 'hello' } } }
-    }
-}
-*/
