8d7414a9fcf8d87d2c6c7833d78ea45b3e9065b2
Mads Nielsen <man@praqma.net>
2017-04-20 13:30:03 +0200

    Added closure getters

--- src/com/praqma/StageGenerator.groovy
+++ src/com/praqma/StageGenerator.groovy
@@ -1,1 +1,13 @@
-'' 
+class StageGenerator {
+    public static String returParameter(String param) {
+        return param
+    }
+
+    public static String getStage() {
+        return "stage('my-stage') { echo 'helloooooeee' } "
+    }
+
+    public static Closure getStageAsClosure() {
+        return { stage("hahah") { echo 'hello' } }
+    }
+}
