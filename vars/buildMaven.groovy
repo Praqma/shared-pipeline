87368fefce82b6ec63a6d3d903d9bfa81dd5472f
Praqma Release User <man@praqma.net>
2018-06-22 09:13:07 +0200

    Use inputs

--- vars/buildMaven.groovy
+++ vars/buildMaven.groovy
@@ -1,5 +1,5 @@
-def call(args = [:]) {
-    docker.image(args.buildImage ?: "maven:3.5.3-jdk-8").inside("-v maven-repo:/root/.m2") {
-        sh args.buildCommand ?: "mvn clean package"
+def call(inputs = [:]) {
+    docker.image(inputs.buildImage ?: "maven:3.5.3-jdk-8").inside("-v maven-repo:/root/.m2") {
+        sh inputs.buildCommand ?: "mvn clean package"
     }    
 }
