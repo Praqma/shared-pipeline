6ab8fffe0c6aaeb18362accf237e00371cb90f11
Praqma Release User <man@praqma.net>
2018-06-25 13:26:55 +0200

    Added pac command

--- vars/pac.groovy
+++ vars/pac.groovy
@@ -1,8 +1,13 @@
 def call(args = [:]) {
 
+    def minFile = "minimal_settings.yml"
     def latestTag = args.latestTag ?: "*"
-    def settingsFile = args.settingsFile ?: "default_settings.yml"
-    def pacArgs = args.pacArgs ?: ""  
+    def settingsFile = args.settingsFile ?: minFile
+    def pacArgs = args.pacArgs ?: ""
+
+    if(!args.settingsFile) {
+        new File(minFile) << new URL("https://raw.githubusercontent.com/Praqma/Praqmatic-Automated-Changelog/master/settings/minimal_settings.yml").getText()
+    }  
 
     docker.image("praqma/pac").inside() {
         sh "from-latest-tag \"$latestTag\" --settings=$settingsFile $pacArgs"
