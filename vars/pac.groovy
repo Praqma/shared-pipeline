bab035a75b3d0e31578ca63ff175e721ad339051
Praqma Release User <man@praqma.net>
2018-06-25 13:35:02 +0200

    Fixup path

--- vars/pac.groovy
+++ vars/pac.groovy
@@ -1,14 +1,15 @@
 def call(args = [:]) {
-
-    def minFile = "minimal_settings.yml"
+    def curDir = pwd()
+    def minFile = "${curDir}/minimal_settings.yml"
     def latestTag = args.latestTag ?: "*"
     def settingsFile = args.settingsFile ?: minFile
     def pacArgs = args.pacArgs ?: ""
 
     if(!args.settingsFile) {
-        new File(minFile) << new URL("https://raw.githubusercontent.com/Praqma/Praqmatic-Automated-Changelog/master/settings/minimal_settings.yml").getText()
-    }  
-
+        echo "Writing minimal settings file..."
+        new File(curDir, minFile) << new URL("https://raw.githubusercontent.com/Praqma/Praqmatic-Automated-Changelog/master/settings/minimal_settings.yml").getText()
+        echo "Done writing minimal settings file..."
+    }          
     docker.image("praqma/pac").inside() {
         sh "from-latest-tag \"$latestTag\" --settings=$settingsFile $pacArgs"
     }
