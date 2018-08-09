02eb9b063bf2cc3f34b868246565757374dc1150
Mads Nielsen <man@praqma.net>
2018-08-09 14:36:50 +0200

    Always use the branch to release from

--- vars/releaseMaven.groovy
+++ vars/releaseMaven.groovy
@@ -5,6 +5,7 @@ def call(args = [:]) {
     withCredentials([usernamePassword(credentialsId: credentialsToUse, passwordVariable: 'passRelease', usernameVariable: 'userRelease'), string(credentialsId: artifactoryUploader, variable: 'RELEASE_PW')]) {
         docker.image(args.buildImage ?: "maven:3.5.3-jdk-8").inside("-v maven-repo:/root/.m2") {
             if(!args.dryRun) {
+                sh 'git checkout -f $BRANCH_NAME'
                 sh 'git config user.email "release@praqma.net" && git config user.name "Praqma Release User"'
                 sh 'mvn clean release:prepare release:perform -B -s settings.xml -Dusername=$userRelease -Dpassword=$passRelease'
             } else {
