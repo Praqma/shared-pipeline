a47c6c18ceaa19635a75b57522b0414f6e98c79b
Mads Nielsen <man@praqma.net>
2018-10-01 14:50:52 +0200

    Import

--- vars/elk.groovy
+++ vars/elk.groovy
@@ -1,5 +1,6 @@
 @GrabExclude(group='org.codehaus.groovy', module='groovy-all')
 @Grab('org.elasticsearch:elasticsearch-groovy:2.1.2')
+import org.elasticsearch.*
 def call() {
   echo 'Hello...we imported elastic searc'
 }
